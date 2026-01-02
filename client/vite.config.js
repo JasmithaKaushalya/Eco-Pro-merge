d5b54c1d850b5739b15e0c48a52b864116ab71a4
unknown <janithkushara@gmail.com>
2026-01-02 15:21:32 +0530

    modify

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-  tailwindcss()],
-})
+    tailwindcss(),
+  ],
+});
